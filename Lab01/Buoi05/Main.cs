85514feaec5c2171837f2cc7d2aa30ae96bac73e
Thiên Phú <phuvanideptrai@gmail.com>
2026-09-15 09:25:52 +0700

    Thay doi vai cho trong Main.cs

--- Lab01/Buoi05/Main.cs
+++ Lab01/Buoi05/Main.cs
@@ -7,10 +7,10 @@ namespace Buoi05
         public static void Main(string[] args)
         {
             // Goi cac bai
-            //Bai01.ChayBai01();
-            //Bai02.ChayBai02();
-            //Bai03.ChayBai03();
-            //Bai04.ChayBai04();
+            Bai01.ChayBai01();
+            Bai02.ChayBai02();
+            Bai03.ChayBai03();
+            Bai04.ChayBai04();
             Bai05.ChayBai05();
         } 
     }
